8bcc4d7e20bc1485657ab27002b65c53112ea4e4
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2020-04-10 12:18:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+// Daniel Shiffman
+// http://youtube.com/thecodingtrain
+// http://codingtra.in
+
+// Coding Challenge #115: Snake Game Redux
+// https://youtu.be/OMoVcohRgZA
+
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake(20,100,10,50);
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === 37) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === 39) {
+    snake.setDir(1, 0);
+  } else if (keyCode === 40) {
+    snake.setDir(0, 1);
+  } else if (keyCode === 38) {
+    snake.setDir(0, -1);
+  } else if (key == '32') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background(220);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
